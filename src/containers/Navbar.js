fe7ab7280dc0c804fb02938878ea138f272295dd
Justin Wasserstein <justin.wasserstein@gmail.com>
2020-10-30 19:23:48 +0000

    Setup redux action creators, reducers, and store.  Create Navbar, Main, and Homepage components and add their styling

---
+++ src/containers/Navbar.js
@@ -0,0 +1,33 @@
+import React, {Component} from 'react';
+import {Link} from 'react-router-dom';
+import {connect} from 'react-redux';
+import Logo from '../images/warbler-logo.png';
+
+class Navbar extends Component {
+	constructor(props){
+		super(props);
+	}
+	render(){
+		return (
+			<nav className='navbar navbar-expand justify-content-between'>
+				<div className='container-fluid'>
+					<Link to='/' className='navbar-brand'>
+						<img src={Logo} alt='Warbler Home' />
+					</Link>
+					<ul className='nav navbar-nav navbar-right'>
+						<li><Link to='/signup'>Sign Up</Link></li>
+						<li><Link to='/signin'>Log In</Link></li>
+					</ul>
+				</div>
+			</nav>
+		);
+	}
+}
+
+function mapStateToProps(state) {
+	return {
+		currentUser: state.currentUser
+	}
+}
+
+export default connect(mapStateToProps, null)(Navbar);
